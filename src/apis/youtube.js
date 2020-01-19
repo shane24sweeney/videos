867f24d58d18cad4ebc102024ce600680bd15e72
Shane James Sweeney <shane24sweeney@yahoo.com>
2020-01-19 10:26:30 -0500

    updates to video project on App and APIs

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+const KEY ='';
+
+export default axios.created({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: KEY
+  }
+})
